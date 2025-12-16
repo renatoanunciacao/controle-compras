001dd2da1017f77e9afed2306b27583db6d62d32
Renato Anunciação <wagnerpunk002@gmail.com>
2025-12-16 14:50:27 -0300

    test(input-button): add unit tests for Input and Button atom

--- src/components/atoms/Input.tsx
+++ src/components/atoms/Input.tsx
@@ -1,16 +1,17 @@
-import { TextField } from "@mui/material";
 import type React from "react";
+import { TextField } from "@mui/material";
 
 interface InputPros {
     label: string;
     value: string | number;
     type?: string;
     placeholder?: string;
+    required?: boolean;
     onChange: (value: string) => void;
     onKeyDown?: (e: React.KeyboardEvent<HTMLInputElement>) => void;
 }
 
-const Input: React.FC<InputPros> = ({ label, value, type, onChange, placeholder, onKeyDown }) => {
+const Input: React.FC<InputPros> = ({ label, value, type, onChange, placeholder, onKeyDown, required }) => {
     return (
         <TextField
             label={label}
@@ -20,7 +21,12 @@ const Input: React.FC<InputPros> = ({ label, value, type, onChange, placeholder,
             onChange={(e) => onChange(e.target.value)}
             onKeyDown={onKeyDown}
             fullWidth
-            required
+            required={required}
+            slotProps={{
+                input: {
+                    required,
+                },
+            }}
         />
     )
 }
